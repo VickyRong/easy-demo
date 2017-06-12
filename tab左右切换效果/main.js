60f5801a31acfc91212042a35c164f23c34fb82e
huyanrong <515020842@qq.com>
2017-06-12 14:22:20 +0800

    优化参数

--- tab左右切换效果/main.js
+++ tab左右切换效果/main.js
@@ -1,15 +1,16 @@
 $(function () {
     var tab = {
+        width:$(".tab-wrap").width(),
+        time:500,
         animation:function(index){
-            var target = -(300 * index);
-            $(".tab-tb").animate({"margin-left":target},700);
+            var target = -(this.width * index);
+            $(".tab-tb").stop().animate({"margin-left":target},this.time);
         },
         thClick: function (index) {
             $(".tab-th > a").eq(index).addClass("active").siblings().removeClass("active");
             this.animation(index);
         },
         init: function () {
-            console.log()
             $(".tab-th > a").on("click", function () {
                 var index = $(this).index();
                 tab.thClick(index);
